158895ef5cb89a728457826ea90e082fa4bf5f42
VishalMakvana7937 <vishalmakvana7937@gmail.com>
2025-01-03 22:00:56 +0530

    insta clone create post and profile

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import '../components/Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='home'>
+      <div className="card">
+
+        <div className="card-header">
+          <div className="card-pic">
+            <img src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png" alt="profile_img" />
+          </div>
+          <h5>Remesh</h5>
+        </div>
+
+        <div className="card-image">
+          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkB8s4QZ7iu5JGCRASoKuaAr8bGoSg2b_VIg&s" alt="post-img" />
+        </div>
+
+        <div className="card-content">
+          <span class="material-symbols-outlined">favorite</span>
+          <p>1 Like</p>
+          <p>This is amaZing</p>
+        </div>
+
+        <div className="add-comment">
+        <span class="material-symbols-outlined">favorite</span>
+          <input type="text" placeholder="Add a comment..." />
+          <button className="comment">Post</button>
+        </div>
+
+      </div>
+    </div>
   )
 }
 
--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,8 +1,37 @@
 import React from 'react'
+import '../components/Profile.css'
 
 const Profile = () => {
   return (
-    <div>Profile</div>
+    <div className='profile'>
+      <div className="profile-frame">
+
+        <div className="profile-pic">
+          <img src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png" alt="profile_img" />
+        </div>
+
+        <div className="profile-data">
+          <h1>Canta Coder</h1>
+          <div className="profile-info" style={{ display: "flex" }}>
+            <p>40 post</p>
+            <p>40 followers</p>
+            <p>40 following</p>
+          </div>
+        </div>
+
+      </div>
+
+      <hr style={{ width: "90%", opacity: "0.8", margin: "25px auto", }} />
+
+      <div className='gallery'>
+        <img src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png" alt="profile_img" />
+        <img src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png" alt="profile_img" />
+        <img src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png" alt="profile_img" />
+        <img src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png" alt="profile_img" />
+        <img src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png" alt="profile_img" />
+        <img src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png" alt="profile_img" />
+      </div>
+    </div>
   )
 }
 
