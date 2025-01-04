2eadda54297d9a4eb14e7a3152edae6021eed3d2
VishalMakvana7937 <vishalmakvana7937@gmail.com>
2025-01-04 22:45:56 +0530

    insta clone create post get api and show home page

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,48 +1,63 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import '../components/Home.css'
 import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
 
   const navigate = useNavigate();
+  const [data, setData] = useState([]);
+
 
   useEffect(() => {
     const token = localStorage.getItem('jwt')
 
-    if(!token){
+    if (!token) {
       navigate("/signup");
     }
+
+    fetch("http://localhost:5000/allposts", {
+      method: "GET",
+      headers: {
+        "Authorization": "Bearer " + localStorage.getItem("jwt")
+      }
+    }).then(res => res.json())
+      .then(result => setData(result))
+      .catch(err => console.log(err))
 
   }, []);
 
   return (
     <div className='home'>
-      <div className="card">
+      {
+        data.map((post) => {
+          return (
+            <div className="card">
+              <div className="card-header">
+                <div className="card-pic">
+                  <img src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png" alt="profile_img" />
+                </div>
+                <h5>{post.postedBy.name}</h5>
+              </div>
 
-        <div className="card-header">
-          <div className="card-pic">
-            <img src="https://png.pngtree.com/png-clipart/20230927/original/pngtree-man-avatar-image-for-profile-png-image_13001882.png" alt="profile_img" />
-          </div>
-          <h5>Remesh</h5>
-        </div>
+              <div className="card-image">
+                <img src={post.photo} alt="post-img" />
+              </div>
 
-        <div className="card-image">
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkB8s4QZ7iu5JGCRASoKuaAr8bGoSg2b_VIg&s" alt="post-img" />
-        </div>
+              <div className="card-content">
+                <span class="material-symbols-outlined">favorite</span>
+                <p>1 Like</p>
+                <p>{post.body}</p>
+              </div>
 
-        <div className="card-content">
-          <span class="material-symbols-outlined">favorite</span>
-          <p>1 Like</p>
-          <p>This is amaZing</p>
-        </div>
-
-        <div className="add-comment">
-        <span class="material-symbols-outlined">favorite</span>
-          <input type="text" placeholder="Add a comment..." />
-          <button className="comment">Post</button>
-        </div>
-
-      </div>
+              <div className="add-comment">
+                <span class="material-symbols-outlined">favorite</span>
+                <input type="text" placeholder="Add a comment..." />
+                <button className="comment">Post</button>
+              </div>
+            </div>
+          )
+        })
+      } 
     </div>
   )
 }
